b3cdbe51ef26b49d44afe14e6a30de3a589d35a6
Tomas Jansson <tomas.jansson@gmail.com>
2025-07-19 00:22:27 +0200

    Update readme with repro

--- app/dynamicstuff/page.tsx
+++ app/dynamicstuff/page.tsx
@@ -1,16 +1,31 @@
 import React, { Suspense } from "react";
 
+const getRandomNumberAsync = async () => {
+  console.log("==> getRandomNumberAsync called");
+  // Simulate a delay
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const randomNumber = Math.floor(Math.random() * 100);
+  console.log("==> Random number generated:", randomNumber);
+  return randomNumber;
+};
+
 const Loader = async () => {
   console.log("==> Loader called");
   // Get random json data from a public API
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-    next: { revalidate: 10 }, // Revalidate every 10 seconds
-  });
+  const randomNumber = await getRandomNumberAsync();
+  console.log("==> Random number fetched:", randomNumber);
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+  console.log("==> Data fetched from API");
   const data = await res.json();
-  return <div>{data.title}</div>;
+  console.log("==> Data fetched:", data);
+  return (
+    <div>
+      {data.title} {randomNumber}
+    </div>
+  );
 };
 
 const Page = () => {
